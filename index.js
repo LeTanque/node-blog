63049d9389587d6d70892c1e3b2b4a63c70e0f12
Frank Martinez <frankie.martinez@gmail.com>
2019-08-08 20:15:52 -0700

    Fixed possible esm error

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require("./server.js");
 
 const port = process.env.PORT || 4000;
+// process.env.port is not there, then do 4000
+// || is a OR operand
+
 const greeting = "Hi, After hours peeps!";
 server.listen(port, () => {
   console.log(
--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,6 @@ module.exports = {
     seeds: {
       directory: './data/seeds',
     },
-  },
+  }
 
 };
